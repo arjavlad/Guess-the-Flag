a92b1b37da4b3ab92887f0efe043147647fcbcd4
Arjav Lad <arjav.webexcellis@gmail.com>
2020-05-06 15:56:11 +0530

    initial commit

---
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Arjav Lad on 06/05/20.
+//  Copyright © 2020 Arjav Lad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
